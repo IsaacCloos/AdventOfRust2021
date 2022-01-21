5a0e6cb17591e7d4e7354b941c6bf0dd4fa69b73
Isaac Cloos <cloosisaac@gmail.com>
2022-01-20 21:38:05 -0500

    dive command mapping

--- dive/src/dive_tools.rs
+++ dive/src/dive_tools.rs
@@ -1,3 +1,47 @@
-pub fn print_name(name: &str) {
-    println!("Hello {name}")
+use std::fs;
+
+// https://dev.to/cthutu/rust-5-naming-conventions-3cjf#casing
+
+// https://doc.rust-lang.org/rust-by-example/custom_types/structs.html
+#[derive(Debug)]
+pub struct DiveCommand(DiveDirection, i32);
+
+// https://doc.rust-lang.org/rust-by-example/conversion/from_into.html
+impl From<&str> for DiveCommand {
+    fn from(item: &str) -> Self {
+        let command_parts = item.split_whitespace().collect::<Vec<&str>>();
+
+        // TODO: research appropriate way to fail a FROM trait function
+        if command_parts.len() != 2 {
+            return DiveCommand(DiveDirection::Forward, 0)
+        }
+
+        let direction = match command_parts[0] {
+            "up" => DiveDirection::Up,
+            "down" => DiveDirection::Down,
+            "forward" => DiveDirection::Forward,
+            _ => panic!("failed to discern direction of dive command"),
+        };
+
+        let distance = str::parse::<i32>(command_parts[1]).expect("failed to measure distance of dive command");
+
+        DiveCommand(direction, distance)
+    }
+}
+
+// https://doc.rust-lang.org/rust-by-example/custom_types/enum.html
+#[derive(Debug)]
+pub enum DiveDirection {
+    Up,
+    Down,
+    Forward,
+}
+
+/// interprets specified text file of dive instructions as DiveCommand(s)
+pub fn parse_dive_instructions_file(input_path: &str) -> Vec<DiveCommand> {
+    fs::read_to_string(input_path)
+        .expect(input_path)
+        .split("\n")
+        .map(DiveCommand::from)
+        .collect()
 }
--- dive/src/main.rs
+++ dive/src/main.rs
@@ -1,5 +1,9 @@
 mod dive_tools;
 
+const INPUT_FILE: &str = "input2.txt";
+
 fn main() {
-    dive_tools::print_name("Isaac")
+    let test = dive_tools::parse_dive_instructions_file(INPUT_FILE);
+
+    println!("{test:#?}")
 }
