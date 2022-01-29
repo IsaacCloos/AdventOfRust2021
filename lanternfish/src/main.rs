b4f95f7018e83b4e4bbfefb9a352bdb0d625817a
Isaac Cloos <cloosisaac@gmail.com>
2022-01-29 14:40:07 -0500

    intro to channels

--- lanternfish/src/main.rs
+++ lanternfish/src/main.rs
@@ -1,53 +1,35 @@
-use std::{fs, process::exit, thread};
+use std::{fs, thread};
 
 const INPUT_PATH: &str = "input_test.txt";
-const DAYS_PASSED: i32 = 80;
-
-const NTHREADS: usize = 5;
-
-// https://doc.rust-lang.org/rust-by-example/std_misc/threads.html
+const DAYS: i32 = 256;
 
 fn main() {
-    let lanternfish_starting_population: Vec<u8> = get_lanternfish_input(INPUT_PATH);
-    //  println!("{} {} {}", lanternfish_starting_population.len(), NTHREADS, (lanternfish_starting_population.len() / NTHREADS) + 1);
-    // exit(0);
-    let mut lanternfish_population_segments: Vec<Vec<u8>> = lanternfish_starting_population
-        .chunks(lanternfish_starting_population.len() / NTHREADS)
-        .map(|x| x.to_owned())
-        .collect();
-    let mut lanternfish_total_count = 0;
+    let lanternfish_initial_population = get_lanternfish_input(INPUT_PATH);
 
-    // good enough for PART ONE
-    // for lanternfish_population_segment in &mut lanternfish_population_segments {
-    //     lanternfish_total_count +=
-    //         simulate_lanternfish_population_growth_v2(lanternfish_population_segment, DAYS_PASSED);
-    // }
-
-    println!("{lanternfish_population_segments:?}");
-
-    // for learning purposes | learn thread/channel basics > optimize PART ONE solution
     let mut children = vec![];
-    for lanternfish_population_segment in lanternfish_population_segments {
+    for lanternfish in lanternfish_initial_population {
         children.push(thread::spawn(move || -> usize {
-            simulate_lanternfish_population_growth_alt(&lanternfish_population_segment, DAYS_PASSED)
-        }));
+            simulate_lanternfish_population_growth(&[lanternfish], DAYS)
+        }))
     }
-    for child in children {
-        lanternfish_total_count += child.join().unwrap();
-    }
+    let final_result = children
+        .into_iter()
+        .map(|c| c.join().unwrap())
+        .sum::<usize>();
 
-    // assert_eq!(26984457539, lanternfish.len());  // input_test * 256 days =  26984457539
-    println!("{lanternfish_total_count}"); // input_test * 80 days  =  5934
+    // let final_result = simulate_lanternfish_population_growth(lanternfish_initial_population.as_slice(), DAYS);
+
+    println!("{final_result:?}")
 }
 
-fn simulate_lanternfish_population_growth_alt(
+fn simulate_lanternfish_population_growth(
     lanternfish_population: &[u8],
     days_remaining: i32,
 ) -> usize {
     println!("{days_remaining}");
     let mut new_list = Vec::<u8>::new();
     for lanternfish in lanternfish_population {
-        if lanternfish == &0 {
+        if *lanternfish == 0 {
             new_list.push(6);
             new_list.push(8);
         } else {
@@ -58,29 +40,8 @@ fn simulate_lanternfish_population_growth_alt(
     if days_remaining == 0 {
         lanternfish_population.len()
     } else {
-        simulate_lanternfish_population_growth_alt(&new_list, days_remaining - 1)
+        simulate_lanternfish_population_growth(&new_list, days_remaining - 1)
     }
-}
-
-fn simulate_lanternfish_population_growth(lanternfish_population: &[u8], days: i32) -> usize {
-    let mut lanternfish: Vec<u8> = Vec::from(lanternfish_population);
-    for num in 0..days {
-        let mut new_lanterfish: usize = 0;
-        for fish in &mut lanternfish {
-            if *fish == 0 {
-                *fish = 6;
-                new_lanterfish += 1;
-            } else {
-                *fish -= 1;
-            }
-        }
-        for _ in 0..new_lanterfish {
-            lanternfish.push(8)
-        }
-        println!("{num}")
-    }
-
-    lanternfish.len()
 }
 
 fn get_lanternfish_input(input_path: &str) -> Vec<u8> {
