0de216dc20e7243394b60f7e85dc87b2a85025de
Isaac Cloos <cloosisaac@gmail.com>
2022-01-29 08:40:42 -0500

    day five part one accepted

---
+++ lanternfish/src/main.rs
@@ -0,0 +1,35 @@
+use std::fs;
+
+const INPUT_PATH: &str = "input.txt";
+const DAYS_PASSED: i32 = 80;
+
+fn main() {
+    let mut lanterfish = get_lanternfish_input(INPUT_PATH);
+
+    for num in 0..DAYS_PASSED {
+        let mut new_lanterfish: usize = 0;
+        for fish in &mut lanterfish {
+            if *fish == 0 {
+                *fish = 6;
+                new_lanterfish += 1;
+            } else {
+                *fish -= 1;
+            }
+        }
+        for _ in 0..new_lanterfish {
+            lanterfish.push(8)
+        }
+        println!("processing day: {num}")
+    }
+
+    println!("{}", lanterfish.len());
+}
+
+fn get_lanternfish_input(input_path: &str) -> Vec<u8> {
+    fs::read_to_string(input_path)
+        .expect("Failed to parse input file")
+        .trim()
+        .split(",")
+        .map(|x| str::parse::<u8>(x).expect("Failed to parse increment of input file to u8"))
+        .collect()
+}
