88e4fdd18bb1f0df60c73fbcec6e9f74549eb934
Isaac Cloos <cloosisaac@gmail.com>
2022-02-08 00:50:55 -0500

    part two in progress

--- smoke_basin/src/main.rs
+++ smoke_basin/src/main.rs
@@ -1,31 +1,69 @@
 use std::fs;
 
-const FILE_PATH: &str = "heightmap.txt";
-
+const FILE_PATH: &str = "heightmap_test.txt";
 
 type HeightMap = Vec<Vec<u32>>;
 
-struct Point {
+trait MapExtensions {
+    fn get_cell_value(&self, point: &Cell) -> u32;
+}
+
+impl MapExtensions for HeightMap {
+    fn get_cell_value(&self, point: &Cell) -> u32 {
+        self[point.y][point.x]
+    }
+}
+
+#[derive(Debug)]
+struct Cell {
     x: usize,
     y: usize,
 }
 
 fn main() {
     let height_map = get_heightmap_from_file(FILE_PATH);
-    let mut sum_risk = 0;
-    for y in 0..height_map.len() {
-        for x in 0..height_map[0].len() {
-            match check_adjacent_locations(&height_map, Point { x, y }) {
-                Some(risk) => sum_risk += risk,
-                None => {
-                    // No behavior
-                }
+    get_regions(&height_map);
+}
+
+fn get_regions(map: &HeightMap) -> Option<Vec<i32>> {
+    let mut basin_edges = Vec::<Cell>::new();
+    let mut basin_edge_groups = Vec::<Vec<Cell>>::new();
+    let length = map[0].len();
+    let height = map.len();
+    for y in 0..map.len() {
+        for x in 0..map[0].len() {
+            let cell = Cell { x, y };
+            let cell_value = map.get_cell_value(&cell);
+            if cell_value == 9 {
+                basin_edges.push(cell);
             }
         }
     }
-    println!("{sum_risk}")
+
+    // println!(
+    //     "{}/{}\n#####################################\n{basin_edges:?}",
+    //     basin_edges.len(),
+    //     length * height
+    // );
+
+    for row in map.iter() {
+        let row_iterator = row
+            .iter()
+            .enumerate()
+            .map(|(i, x)| (x, i))
+            .collect::<Vec<(_, _)>>();
+        let basin_segments = row_iterator
+            .split(|number| *number.0 == 9)
+            .filter(|elm| elm.len() > 0)
+            .collect::<Vec<_>>();
+
+        println!("{:?}", basin_segments);
+    }
+
+    None
 }
-fn check_adjacent_locations(map: &HeightMap, cell: Point) -> Option<u32> {
+
+fn check_adjacent_locations(map: &HeightMap, cell: Cell) -> Option<u32> {
     let cell_value = map[cell.y][cell.x];
     let length = map[0].len() - 1;
     let height = map.len() - 1;
@@ -49,6 +87,7 @@ fn check_adjacent_locations(map: &HeightMap, cell: Point) -> Option<u32> {
     }
     Some(1 + cell_value)
 }
+
 fn get_heightmap_from_file(file_path: &str) -> HeightMap {
     fs::read_to_string(file_path)
         .expect("Failed to read file")
