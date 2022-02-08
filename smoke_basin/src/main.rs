1b1e16bdede64c596c2ef6f4dfb5e906a66a8d6b
Isaac Cloos <cloosisaac@gmail.com>
2022-02-08 17:32:15 -0500

    checking orphans of larger set

--- smoke_basin/src/main.rs
+++ smoke_basin/src/main.rs
@@ -1,6 +1,6 @@
 use std::{collections::HashSet, fs};
 
-const FILE_PATH: &str = "heightmap_test.txt";
+const FILE_PATH: &str = "heightmap.txt";
 
 type HeightMap = Vec<Vec<u32>>;
 
@@ -22,12 +22,16 @@ struct Cell {
 
 fn main() {
     let height_map = get_heightmap_from_file(FILE_PATH);
-    get_regions(&height_map);
+    let result = get_regions(&height_map);
+
+    // 750975 is too low
+
+    // println!("{result}")
 }
 
-fn get_regions(map: &HeightMap) -> Option<Vec<i32>> {
+fn get_regions(map: &HeightMap) -> usize {
     let mut basin_segments = Vec::<Vec<(Vec<(&u32, usize)>, bool)>>::new();
-    let mut basin_segment_sizes = Vec::<(usize, (usize, HashSet<usize>))>::new();
+    let mut basin_segment_sizes = Vec::<(usize, Vec<(usize, HashSet<usize>)>)>::new();
 
     for row in map.into_iter() {
         let row_iterator = row
@@ -69,41 +73,36 @@ fn get_regions(map: &HeightMap) -> Option<Vec<i32>> {
             let mut match_found = false;
             let mut matched_index = 0;
 
-
             // integration check
 
             // for all segments collected already
-            for (i1, collected_segments) in
-                basin_segment_sizes.iter_mut().enumerate()
-            {
+            for (i1, collected_segments) in basin_segment_sizes.iter_mut().enumerate() {
                 // 0 is y axis
-                let previous_segment = &mut collected_segments.1;
-
-                // hashset of indexes on y axis
-                let previous_indexes = &mut previous_segment.1;
+                let previous_segments = &mut collected_segments.1;
 
                 for index in index_set.iter() {
-                    for previous_index in previous_indexes.iter() {
-                        if index == previous_index && previous_segment.0 as i32 == (i as i32 - 1 as i32) {
-                            match_found = true;
-                            matched_index = i1;
+                    for previous_segment in previous_segments.iter_mut() {
+                        for previous_index in previous_segment.1.iter() {
+                            if index == previous_index
+                                && previous_segment.0 as i32 == (i as i32 - 1 as i32)
+                            {
+                                match_found = true;
+                                matched_index = i1;
+                            }
                         }
                     }
                 }
-
             }
-            
 
             // apply and mark segment as completed
             if match_found {
                 let matched_basin = basin_segment_sizes.get_mut(matched_index).unwrap();
                 matched_basin.0 += segment.0.len();
-                
+
                 let matched_basin_definition = &mut matched_basin.1;
-                matched_basin_definition.0 = i;
-                matched_basin_definition.1 = index_set;
+                matched_basin_definition.push((i, index_set));
             } else {
-                basin_segment_sizes.push((segment.0.len(), (i, index_set)));
+                basin_segment_sizes.push((segment.0.len(), Vec::from([(i, index_set)])));
             }
 
             // mark segment as integrated
@@ -111,9 +110,18 @@ fn get_regions(map: &HeightMap) -> Option<Vec<i32>> {
         }
     }
 
-    println!("{basin_segment_sizes:?}");
+    basin_segment_sizes.sort_by(|a, b| b.0.cmp(&a.0));
 
-    None
+    let mut result: usize = 1;
+
+    for defined_segment in &basin_segment_sizes {
+        if defined_segment.0 == 1 {
+            println!("{:?}", defined_segment);
+        }
+        // result *= defined_segment.0;
+    }
+
+    result
 }
 
 fn get_heightmap_from_file(file_path: &str) -> HeightMap {
